caa868faaa63484386becb290c006a97afe3f5c1
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-05-25 19:27:35 +0530

    Declare Interface and Dependency Injuction

---
+++ BusinessLayer/Interface/IParkingBL.cs
@@ -0,0 +1,12 @@
+﻿using CommonLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface IParkingBL
+    {
+        bool ParkingLoginDatails(ParkingUser Info);
+    }
+}
---
+++ BusinessLayer/Services/ParkingBL.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Services;
+using RepositoryLAyer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    public class ParkingBL : IParkingBL
+    {
+        private IParkingRL Parking;
+        public ParkingBL(IParkingRL data)
+        {
+            Parking = data;
+        }
+        public bool ParkingLoginDatails(ParkingUser Info)
+        {
+            try
+            {
+                var Result = Parking.ParkingLoginDatails(Info);
+                if (!Result.Equals(null))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
