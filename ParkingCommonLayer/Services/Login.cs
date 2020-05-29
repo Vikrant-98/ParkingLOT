c1acf36c598ebb110cba97ae4a36b78b3a22c290
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-05-29 17:27:02 +0530

    Created models for Login and Registration

---
+++ ParkingCommonLayer/Services/Login.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkingCommonLayer.Services
+{
+    public class Login
+    {
+        [Required]
+        [RegularExpression(@"^([a-zA-Z0-9]{2}[a-zA-Z0-9]*[.]{0,1}[a-zA-Z0-9]*@[a-zA-Z0-9]*.{1}[a-zA-Z0-9]*[.]*[a-zA-Z0-9]*)$", ErrorMessage = "Enter Valid Email")]
+        //Mail ID
+        public string MailID { get; set; }
+        [Required(ErrorMessage = "Password Is Required")]
+        [RegularExpression(@"^.{8,15}$", ErrorMessage = "Password Length should be between 8 to 15")]
+        //Password
+        public string Password { get; set; }
+    }
+}
---
+++ ParkingCommonLayer/Services/ParkingUser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ParkingCommonLayer.Services
+{
+    [Table("User")]
+    public class ParkingUser
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //Unique ID get generated using auto Increment
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [RegularExpression(@"^[A-Z]{1}[a-z]{2}[a-z]*$", ErrorMessage = "Enter Valid First Name")]
+        //First Name
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [RegularExpression(@"^[A-Z]{1}[a-z]{2}[a-z]*$", ErrorMessage = "Enter Valid Last Name")]
+        //Last Name
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"^([a-zA-Z0-9]{2}[a-zA-Z0-9]*[.]{0,1}[a-zA-Z0-9]*@[a-zA-Z0-9]*.{1}[a-zA-Z0-9]*[.]*[a-zA-Z0-9]*)$", ErrorMessage = "Enter Valid Email")]
+        //Mail ID
+        public string MailID { get; set; }
+        [Required(ErrorMessage = "DriverCategory Is Required")]
+        [MaxLength(50)]
+        //Driver Categiry
+        public string DriverCategory { get; set; }
+        [Required(ErrorMessage = "Password Is Required")]
+        [RegularExpression(@"^.{8,15}$", ErrorMessage = "Password Length should be between 8 to 15")]
+        //Password
+        public string Password { get; set; }
+        //Create date 
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        //Modified Date
+        public DateTime ModifiedDate { get; set; }
+    }
+    public class EncryptedPassword
+    {
+        /// <summary>
+        /// Encrypt the Password logic
+        /// </summary>
+        /// <param name="Password"></param>
+        /// <returns></returns>
+        public static string EncodePasswordToBase64(string Password)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[Password.Length];
+                encData_byte = Encoding.UTF8.GetBytes(Password);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;                                         //Return Encrypted Data
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
+        }
+    }
+}
