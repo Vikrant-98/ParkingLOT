d232b64e27558e8e7bb96e7fc29ed55bb900c1b1
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-06-09 20:25:13 +0530

    Added : connection with Gmail using username and password

---
+++ MessageListner/MessageListner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MessageListner
+{
+    public class MessageListner
+    {
+        public static void Main()
+        {
+            var listener = new MSMQListener(@".\Private$\myqueue");
+            listener.MessageReceived += new MessageReceivedEventHandler(listener_MessageReceived);
+            listener.Start();
+            Console.WriteLine("Read Message");
+            Console.ReadLine();
+            listener.Stop();
+        }
+
+        public static void listener_MessageReceived(object sender, MessageEventArgs args)
+        {
+            Console.WriteLine(args.MessageBody);
+        }
+    }
+}
---
+++ MessageListner/SMTP.cs
@@ -0,0 +1,41 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+
+namespace MessageListner
+{
+    public class SMTP
+    {
+        //SMTP method to send mail
+        public void SendMail(string name, string mail, string data)
+        {
+            try
+            {
+                var message = new MimeMessage();
+
+                message.From.Add(new MailboxAddress(mail));
+
+                message.To.Add(new MailboxAddress("Parking Lot", "chittevikey5@gmail.com"));
+
+                message.Subject = "Registration";
+
+                message.Body = new TextPart("plain")
+                {
+                    Text = data
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.Authenticate("chittevikey55@gmail.com", "vik98CH!");
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
